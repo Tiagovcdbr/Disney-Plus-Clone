45470561e73f07e4cd1f1687405f4f8923252c59
Tiago <ti.volei@hotmail.com>
2021-08-11 17:44:34 -0300

    configurando eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,22 @@ function Header() {
         <Nav>
             <Logo src="/images/logo.svg" />
             <NavMenu>
-
+                <a>
+                    <img alt="home" src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img alt="search" src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img alt="watchlist" src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img alt="original" src="/images/original-icon.svg" />
+                    <span>ORIGINAL</span>
+                </a>
 
             </NavMenu>
         </Nav>
@@ -28,5 +43,21 @@ const Logo = styled.img`
 `
 
 const NavMenu = styled.div`
+    display: flex;
 
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
 `
